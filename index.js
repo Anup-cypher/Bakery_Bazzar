4766d2d13e334b314bfd1be994297b672f5efa00
anup <anuppanta5555@gmail.com>
2021-01-03 08:46:54 +0545

    core http server example

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const url = require('url');
 
 let server = http.createServer((request, response)=>{
+	// console.log(request.url);
+	// let qureyObject = url.parse(request.url, true).query;
+	// console.log(qureyObject);
+	// response.writeHead(200, {'content-type': 'text/html'});
+	// response.write(`<html><body><h1>Hello ${qureyObject.name}</h1><p>This is root page. His age is ${qureyObject.age}</p></body></html>`);
+	// response.end();
 	if(request.url === '/'){
 		response.writeHead(200, {'content-type': 'text/html'});
 		response.write('<html><body><h1>Hello world</h1><p>This is root page.</p></body></html>');
 		response.end();
 	}else if(request.url === '/profile'){
 		response.writeHead(200, {'content-type': 'text/html'});
-		response.write('<html><body><h1>Hello IIMS</h1><p>This is profile page.</p></body></html>');
+		response.write('<html><body><h1>Hello IIMS.</h1><p>This is profile page.</p></body></html>');
 		response.end();
 
 	}
